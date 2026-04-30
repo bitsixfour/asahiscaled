48dc0426cda212d48e084730ebb8d53ee4471ea3
will <me@wngyn.net>
2026-04-29 21:31:35 -0500

    graph

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 use crossterm::event::{self, Event, KeyCode, KeyEventKind};
 use ratatui::{
-    layout::{Constraint, Direction, Layout},
-    style::{Color, Style},
-    widgets::{Block, Borders, Paragraph, Wrap},
+    layout::{Constraint, Direction, Layout, Rect},
+    style::{Color, Style, Stylize},
+    widgets::{Block, Chart, Dataset, GraphType, Borders, Axis, Paragraph, Wrap},
     DefaultTerminal, Frame,
 };
-use std::time::Duration;
 
+use std::time::Duration;
+use ratatui::symbols::Marker;
 mod sens;
 use sens::Sens;
 
@@ -29,6 +30,7 @@ fn run(terminal: &mut DefaultTerminal) -> Result<()> {
 }
 
 fn render(frame: &mut Frame, sens: &Sens) {
+    // refactor some point
     let pressure = sens.get_pressure();
     let area = frame.area();
 
@@ -60,16 +62,15 @@ fn render(frame: &mut Frame, sens: &Sens) {
 
 
 
-
+   // top
     let weight = sens.calc_weight();
     let text = format!(
-        "Device: {}\nPressure: {}\nFolder: /dev/input/event2
+        "Device: {}  | Pressure: {} Status: Runing \nFolder: /dev/input/event2
         Weight: {} kg \n
-        Status: running\m
         State: {}
         \n\nPress 'q' to quit.",
 
-        sens.name, pressure, weight
+        sens.name, pressure, weight, get_status(sens)
     );
     let paragraph = Paragraph::new(text).wrap(Wrap { trim: true });
     frame.render_widget(paragraph, top_inner);
@@ -86,6 +87,40 @@ fn render(frame: &mut Frame, sens: &Sens) {
 
     let footer = Paragraph::new(para_two).wrap(Wrap { trim: true });
     frame.render_widget(footer, bottom_inner);
+    render_chart(frame, middle_inner);
+}
+
+pub fn render_chart(frame: &mut Frame, area: Rect) {
+    let dataset = Dataset::default()
+        .name("Stonks")
+        .marker(Marker::Braille)
+        .graph_type(GraphType::Line)
+        .style(Color::Blue)
+        .data(&[
+            (0.0, 1.0),
+            (1.0, 3.0),
+            (2.0, 0.5),
+            (3.0, 2.0),
+            (4.0, 0.8),
+            (5.0, 4.0),
+            (6.0, 1.0),
+            (7.0, 6.0),
+            (8.0, 3.0),
+            (10.0, 10.0),
+        ]);
+
+    let x_axis = Axis::default()
+        .title("Hustle".blue())
+        .bounds([0.0, 10.0])
+        .labels(["0%", "50%", "100%"]);
+
+    let y_axis = Axis::default()
+        .title("Profit".blue())
+        .bounds([0.0, 10.0])
+        .labels(["0", "5", "10"]);
+
+    let chart = Chart::new(vec![dataset]).x_axis(x_axis).y_axis(y_axis);
+    frame.render_widget(chart, area);
 }
 fn should_quit() -> Result<bool> {
     if event::poll(Duration::from_millis(125))? {
